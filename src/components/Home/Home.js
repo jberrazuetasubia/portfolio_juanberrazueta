b68f9d378b3b828ccd8dbb3757aedf69e5313c08
jberrazuetasubia <j_berrazuetasubia@fanshaweonline.ca>
2024-11-26 12:09:40 -0500

    adding some things

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
+
 import './Home.scss';
 
 const Home = () => {
     return (
         <>
             <div className="home">
-                
+
                 div
                 <div className='presentation-wrapper'>
                     <div class='row'>
@@ -15,7 +17,7 @@ const Home = () => {
                                 <h1>Hi!</h1>
                                 <h2>My name is Juanfer</h2>
                                 <h3>I build interactive content for your projects</h3>
-                           
+                                <button className='explore-button'>Explore my Work</button>
                             </div>
                         </div>
                     </div>
@@ -23,7 +25,14 @@ const Home = () => {
                         <div class='column'>
                             <div className='presentation-image'>
                                 < h1>Htessssi!</h1>
-                              </div>
+                                <Typewriter
+                                    options={{
+                                        strings: ['Hello my name is', 'Juanfer'],
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </div>
                         </div>
                     </div>
 
